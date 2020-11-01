071bffb5489af5b07c3bff518e04bb2a5439bdac
Ivan Holovach <iholovach@amplify.com>
2020-11-01 21:04:49 +0200

    implemented calendar-schema library

---
+++ REDME.md
@@ -0,0 +1,47 @@
+## Calendar-schema v1.0.0
+## Installation:
+``npm i --save calendar-schema``
+##Example:
+``````
+import calendarSchema from 'calendar-schema';
+
+const month = 1;
+const year = 2021;
+const daysSchemaforCalendar = calendarSchema(month, year);
+
+    // output => [ { day: 28, month: 11, year: 2020, order: 0 },
+    //             { day: 29, month: 11, year: 2020, order: 1 },
+    //             { day: 30, month: 11, year: 2020, order: 2 },
+    //             { day: 31, month: 11, year: 2020, order: 3 },
+    //             { day: 1, month: 0, year: 2021, order: 5 },
+    //             { day: 2, month: 0, year: 2021, order: 6 },
+    //             { day: 3, month: 0, year: 2021, order: 7 },
+    //             { day: 4, month: 0, year: 2021, order: 8 },
+    //             { day: 5, month: 0, year: 2021, order: 9 },
+    //             { day: 6, month: 0, year: 2021, order: 10 },
+    //             { day: 7, month: 0, year: 2021, order: 11 },
+    //             { day: 8, month: 0, year: 2021, order: 12 },
+    //             { day: 9, month: 0, year: 2021, order: 13 },
+    //             { day: 10, month: 0, year: 2021, order: 14 },
+    //             { day: 11, month: 0, year: 2021, order: 15 },
+    //             { day: 12, month: 0, year: 2021, order: 16 },
+    //             { day: 13, month: 0, year: 2021, order: 17 },
+    //             { day: 14, month: 0, year: 2021, order: 18 },
+    //             { day: 15, month: 0, year: 2021, order: 19 },
+    //             { day: 16, month: 0, year: 2021, order: 20 },
+    //             { day: 17, month: 0, year: 2021, order: 21 },
+    //             { day: 18, month: 0, year: 2021, order: 22 },
+    //             { day: 19, month: 0, year: 2021, order: 23 },
+    //             { day: 20, month: 0, year: 2021, order: 24 },
+    //             { day: 21, month: 0, year: 2021, order: 25 },
+    //             { day: 22, month: 0, year: 2021, order: 26 },
+    //             { day: 23, month: 0, year: 2021, order: 27 },
+    //             { day: 24, month: 0, year: 2021, order: 28 },
+    //             { day: 25, month: 0, year: 2021, order: 29 },
+    //             { day: 26, month: 0, year: 2021, order: 30 },
+    //             { day: 27, month: 0, year: 2021, order: 31 },
+    //             { day: 28, month: 0, year: 2021, order: 32 },
+    //             { day: 29, month: 0, year: 2021, order: 33 },
+    //             { day: 30, month: 0, year: 2021, order: 34 },
+    //             { day: 31, month: 0, year: 2021, order: 35 } ]
+````
---
+++ index.js
@@ -0,0 +1,115 @@
+const ERROR_MESSAGE = 'Please send correct args';
+const SMALL_CALENDAR_DAYS_COUNT = 35;
+const LARGE_CALENDAR_DAYS_COUNT = 42;
+
+function getFirstDayInMonth(year, month) {
+    return new Date(year, month).getDay();
+}
+
+function getDaysInMonth(iMonth, iYear) {
+    const day = new Date(iYear, iMonth, 32).getDate();
+    return 32 - day;
+}
+
+function getPreviousMonthDays(currentMonth, currentYear) {
+    const year = currentMonth === 0 ? currentYear - 1 : currentYear;
+    const prevMonthNumber = currentMonth === 0 ? 11 : currentMonth - 1;
+    const firstDayInMonth = getFirstDayInMonth(currentYear, currentMonth);
+    const prevMonthDays = getDaysInMonth(prevMonthNumber, currentYear);
+    const prevMonthDaysLeft = firstDayInMonth === 0 ? 6 : firstDayInMonth - 1;
+
+    return new Array(prevMonthDaysLeft)
+        .fill(null)
+        .reduce((result, item, index) => {
+            const prevMonthExtraDays = prevMonthDays + 1;
+            const day = prevMonthExtraDays - (prevMonthDaysLeft - index);
+
+            result.push({
+                day,
+                month: prevMonthNumber,
+                year: year,
+                order: index,
+            });
+            return result;
+        }, []);
+}
+
+function getCurrentMonthDays(currentMonth, currentYear, calendarDays) {
+    const currentMonthDays = getDaysInMonth(currentMonth, currentYear);
+
+    return new Array(currentMonthDays)
+        .fill(null)
+        .reduce((result, item, index) => {
+            const day = index + 1;
+            const order = calendarDays.length + index + 1;
+
+            result.push({ day, month: currentMonth, year: currentYear, order });
+            return result;
+        }, []);
+}
+
+function getNextMonthDays(currentMonth, currentYear, calendarDays) {
+    const year = currentMonth === 11 ? currentYear + 1 : currentYear;
+    const maxCalendarDays =
+        calendarDays.length > SMALL_CALENDAR_DAYS_COUNT
+            ? LARGE_CALENDAR_DAYS_COUNT - calendarDays.length
+            : SMALL_CALENDAR_DAYS_COUNT - calendarDays.length;
+
+    return new Array(maxCalendarDays)
+        .fill(null)
+        .reduce((result, item, index) => {
+            const newMonth = currentMonth === 11 ? 0 : currentMonth + 1;
+            const day = index + 1;
+            const order = calendarDays.length + day;
+
+            result.push({ day, month: newMonth, year, order });
+            return result;
+        }, []);
+}
+
+function checkIsNotValidParams(currentMonth, currentYear) {
+    return typeof currentMonth !== 'number' || typeof currentYear !== 'number';
+}
+
+function getNormalizedArgs(currentMonth, currentYear) {
+    return {
+        normalizedCurrentMonth: +currentMonth + 1,
+        normalizedCurrentYear: +currentYear,
+    };
+}
+
+function calendarSchema(currentMonth, currentYear) {
+    const { normalizedCurrentMonth, normalizedCurrentYear } = getNormalizedArgs(
+        currentMonth,
+        currentYear
+    );
+    const isNotValid = checkIsNotValidParams(
+        normalizedCurrentMonth,
+        normalizedCurrentYear
+    );
+
+    if (isNotValid) return ERROR_MESSAGE;
+
+    const calendarDays = [];
+    calendarDays.push(
+        ...getPreviousMonthDays(normalizedCurrentMonth, normalizedCurrentYear)
+    );
+    calendarDays.push(
+        ...getCurrentMonthDays(
+            normalizedCurrentMonth,
+            normalizedCurrentYear,
+            calendarDays
+        )
+    );
+    calendarDays.push(
+        ...getNextMonthDays(
+            normalizedCurrentMonth,
+            normalizedCurrentYear,
+            calendarDays
+        )
+    );
+
+    return calendarDays.sort((a, b) => (a.order > b.order ? 1 : -1));
+}
+
+module.exports = calendarSchema;
